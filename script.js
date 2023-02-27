b7109fa687567c4080dc95d68b0ba47aae99780d
Arthur <predko.artur93@gmail.com>
2023-02-27 02:14:31 +0200

    add cycles

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const numberOfFilms = +prompt ('Cкільки фільмів ви вже подивились', '');
+'use strict'
 
+const numberOfFilms = +prompt('Cкільки фільмів ви вже подивились', '');
+
+//.......................Data base
 const prersonalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -8,12 +11,27 @@ const prersonalMovieDB = {
     privat: false
 };
 
-const a = prompt ('Один із останніх переглянутих фільмів?', ''),
-      b = prompt ('Яка ваша оцінка йому?', ''),
-      с = prompt ('Один із останніх переглянутих фільмів?', ''),
-      d = prompt ('Яка ваша оцінка йому?', '');
+// ....................right writing
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один із останніх переглянутих фільмів?', ''),
+        b = prompt('Яка ваша оцінка йому?', '');
 
-prersonalMovieDB.movies[a] = b;
-prersonalMovieDB.movies[с] = d;
+    if (a !== '' && b !== '' && a!== null && b!== null && a.length < 50 && b.length !== 50){
+        prersonalMovieDB.movies[a] = b;
+    } else{
+        console.log('помилка!');
+        i--
+    }  
+}
 
+// ................kind of watcher
+if (prersonalMovieDB.count < 10){
+    console.log('Замало пидивились(');
+} else if (prersonalMovieDB.count >= 10 && prersonalMovieDB.count < 30) {
+    console.log('Ви класичний глядач)');
+} else if (prersonalMovieDB.count > 30) {
+    console.log('Ви кіноман)');
+} else {
+    console.log('ПОМИЛКА!(((');
+}
 console.log(prersonalMovieDB);
